a500345a4e650d8aa534562afe303ead28b35303
muhananaufal <muhananaufal8@gmail.com>
2025-07-16 19:57:44 +0700

    fix: coaching programs

--- src/components/fragments/graduation-report.tsx
+++ src/components/fragments/graduation-report.tsx
@@ -55,7 +55,7 @@ const itemVariants = {
 		opacity: 1,
 		transition: { type: 'spring', stiffness: 100 },
 	},
-};
+} as const;
 
 export function GraduationDialogContent({ details }: GraduationDialogProps) {
 	const [showConfetti, setShowConfetti] = useState(true);
